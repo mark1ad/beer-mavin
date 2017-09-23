5358ad4bea079348a4a9a2f8ce2a6313d0db84ac
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-09-23 09:18:23 -0600

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <h1>Beer rater</h1>
+    </div>
+  )
+}
+
+export default Header;
