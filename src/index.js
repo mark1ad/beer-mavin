1f3bcb08cb2856cb525e3300fd9c7b4ab53ed8c8
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-11-03 09:23:21 -0600

    Forgot to include index.js in previous commit.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import axios from 'axios';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import storeFactory from './components/storeFactory';
+import { addBeers } from './components/actions';
+
+const loadData = (store) => {
+  const address = "http://localhost:3000/beers";
+  axios.get(address)
+    .then( res => {
+      store.dispatch(addBeers(res.data))
+    })
+}
+
 
 const store = storeFactory();
-
+// TODO: move loading store to more appriopriate place
+loadData(store)
 ReactDOM.render(
   <Provider store={store}>
     <App />
