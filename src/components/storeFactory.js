ece154f78b3ac1e9062c279b9d6ea8dc01c7ef84
Mark De May <markd@frii.com>
2017-11-27 11:29:13 -0700

    Handle displaying multiple breweries for a beer. Collaboration beers can have multiple breweries.

--- src/components/storeFactory.js
+++ src/components/storeFactory.js
@@ -10,12 +10,14 @@ let initStore = {
 };
 
 const logger = store => next => action => {
-  console.groupCollapsed("dispatching ", action.type)
+  // console.groupCollapsed("dispatching ", action.type)
+  console.log('\n>>> Dispatching ', action.type);
   console.log('prev state ', store.getState());
   console.log('action ', action);
   next(action)
   console.log('next state ', store.getState());
-  console.groupEnd();
+  console.log('\n');
+  // console.groupEnd();
 }
 
 const storeFactory = (initialState = initStore) =>
