ba13d3580c7973865aaeb14524f53f3129eef6f1
Mark De May <markd@frii.com>
2017-11-28 11:47:19 -0700

    Content and basic layout for footer.

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p className="text-muted">Place sticky footer content here.</p>
+        <div className="row">
+          <div className="col-xs-6">
+            <a href="about">About</a>
+          </div>
+          <div className="col-xs-6">
+            <span className="pull-right">&copy; <a href="http://www.markdemay.com">Mark De May</a></span>
+          </div>
+        </div>
       </div>
     </footer>
   )
